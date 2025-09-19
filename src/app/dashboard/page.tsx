6751d925001b85a622476249e1b2750c9a94df1b
Hamide hadeny <www.poronpath@gmail.com>
2025-09-19 09:38:20 +0000

    Runtime Error
    TypeError: Super expression must either be null or a fun

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { ArrowLeftRight, DollarSign, ReceiptText, PlusCircle } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
